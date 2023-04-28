fc5692a95c356d088ac6a63b95b25b78c1c3e075
Dante Cosme <dantedcosme@gmail.com>
2023-04-28 10:54:32 -0400

    created ProductsController class and added appropriate properties & method task 4 complete. defining products.html next

---
+++ resources/scripts/itemsController.js
@@ -0,0 +1,31 @@
+class ProductsController {
+    constructor(currentId = 0) {
+        this._currentId = currentId;
+        this._items = [];
+    }
+
+    get currentId() {
+        return this.currentId;
+    }
+
+    get items() {
+        return this._items;
+    }
+
+    addItem(name, description, img, createdAt) {
+        this._currentId++;
+        const newItem = {
+            id: this._currentId,
+            name: name,
+            description: description,
+            img: img,
+            createdAt: createdAt,
+        };
+        this._items.push(newItem);
+    }
+}
+
+/* testing code
+const productArrayTest = new ProductsController();
+productArrayTest.addItem('Test product', 'This is the test description of my new product item', './resources/img/huawei.jpg', '04-28-2023');
+console.log(productArrayTest.items) */
